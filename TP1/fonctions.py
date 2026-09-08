00ea0dfc1859adda8cccbf57f7c495482d402c0c
Your Name <you@example.com>
2026-09-08 16:31:30 +0200

    Ajout des tests des cas limites

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,9 +1,12 @@
-def puissance(a, b):
+def puiss(a, b):
     if not type(a) is int:
         raise TypeError("Only integers are allowed")
-    
+
     if not type(b) is int:
         raise TypeError("Only integers are allowed")
-    
+
+    if a == 0 and b < 0:
+        raise ValueError("Undefined operation")
+
     return a ** b
 
--- TP1/test.py
+++ TP1/test.py
@@ -2,11 +2,21 @@ import pytest
 import fonctions as f
 
 def test_1():
-	assert f.puissance(2,3) == 8
-	assert f.puissance(2,2) == 4
+    assert f.puiss(2,3) == 8
+    assert f.puiss(2,2) == 4
+
 def test_2():
-    assert f.puissance(-1,2) == 1
-    assert f.puissance(-1,3) == -1
-    assert f.puissance(-1,-1) == -1
-    assert f.puissance(-1,-2) == 1
-    assert f.puissance(-2,-1) == -0.5
+    assert f.puiss(-1,2) == 1
+    assert f.puiss(-1,3) == -1
+    assert f.puiss(-1,-1) == -1
+    assert f.puiss(-1,-2) == 1
+    assert f.puiss(-2,-1) == -0.5
+
+def test_3():
+    assert f.puiss(0,1) == 0
+    assert f.puiss(0,2) == 0
+    assert f.puiss(0,10) == 0
+
+    with pytest.raises(ValueError):
+        f.puiss(0,-1)
+
